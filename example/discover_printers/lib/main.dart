9ad7dbef48719c55b40479158205582c92be301d
Victor Flores <victorflores20511@gmnail.com>
2023-10-23 10:30:59 -0500

    Termine

--- example/discover_printers/lib/main.dart
+++ example/discover_printers/lib/main.dart
@@ -1,7 +1,6 @@
 
 
-
-import 'package:esc_pos_printer/esc_pos_printer.dart';
+import 'package:esc_pos_printer_forked_v/esc_pos_printer.dart';
 import 'package:flutter/material.dart' hide Image;
 import 'package:flutter/services.dart';
 import 'package:flutter_esc_pos_utils/flutter_esc_pos_utils.dart';
